afc4b3dd8928468cc7650cae711c0cb1ca653482
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-03-02 21:29:54 +0100

    Update navigation.tsx

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,27 +1,27 @@
-import Link from 'next/link';
+// import Link from 'next/link';
 
-const Navigation: React.FC = () => {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <Link href="/">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link href="/password">
-            Password Generator
-          </Link>
-        </li>
-        <li>
-          <Link href="/strength">
-            Password Strength Tester
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  );
-};
+// const Navigation: React.FC = () => {
+//   return (
+//     <nav>
+//       <ul>
+//         <li>
+//           <Link href="/">
+//             Home
+//           </Link>
+//         </li>
+//         <li>
+//           <Link href="/password">
+//             Password Generator
+//           </Link>
+//         </li>
+//         <li>
+//           <Link href="/strength">
+//             Password Strength Tester
+//           </Link>
+//         </li>
+//       </ul>
+//     </nav>
+//   );
+// };
 
-export default Navigation;
+// export default Navigation;
