02385acff8a0f09e94b44d97770e4f166e2247f9
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-03-09 20:58:16 +0100

    mark active link

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,34 +1,36 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+
 interface NavItem {
   name: string;
   href: string;
-  current: boolean;
 }
 
 const navigation: NavItem[] = [
-  { name: 'Home', href: '/', current: true },
-  { name: 'Password Generator', href: '/password', current: false },
-  { name: 'Password Strength Tester', href: '/strength', current: false },
+  { name: 'Home', href: '/' },
+  { name: 'Password Generator', href: '/password' },
+  { name: 'Password Strength Tester', href: '/strength' },
 ];
 
 const Navigation = () => {
+  const router = useRouter();
 
   return (
-		<div className='flex justify-evenly'>
-			{navigation.map((item) => (
-				<Link
-				key={item.name}
-				href={item.href}
-				className={`${
-					item.current ? 'font-medium text-gray-900' : 'text-gray-500'
-				} hover:text-gray-900 block px-3 py-2 rounded-md text-base font-medium`}
-				aria-current={item.current ? 'page' : undefined}
-				>
-				{item.name}
-				</Link>
-			))}
-		</div>
-	);
+    <div className='flex justify-evenly'>
+      {navigation.map((item) => (
+        <Link
+          key={item.name}
+          href={item.href}
+          className={`${
+            router.pathname === item.href ? 'font-medium text-gray-900' : 'text-gray-500'
+          } hover:text-gray-900 block px-3 py-2 rounded-md text-base font-medium`}
+          aria-current={router.pathname === item.href ? 'page' : undefined}
+        >
+          {item.name}
+        </Link>
+      ))}
+    </div>
+  );
 };
-      
+
 export default Navigation;
